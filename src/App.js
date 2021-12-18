23c8a3579e6988a4375395cf3523fa026011edef
foses <frdeoses@outlook.es>
2021-12-18 14:09:01 +0100

    import Pelicula and add component Pelicula

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Pelicula from './Pelicula';
 
 function App() {
   return (
@@ -228,30 +229,23 @@ function App() {
 				</div>
 
 
-				<div className="movie-item-style-2">
-					<img src="images/uploads/mv1.jpg" alt="" />
-					<div className="mv-item-infor">
-						<h6><a href="moviesingle.html">oblivion <span>(2012)</span></a></h6>
-						<p className="rate"><i className="ion-android-star"></i><span>8.1</span> /10</p>
-						<p className="describe">Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...</p>
-						<p className="run-time"> Run Time: 2h21’    .     <span>MMPA: PG-13 </span>    .     <span>Release: 1 May 2015</span></p>
-						<p>Director: <a href="#">Joss Whedon</a></p>
-						<p>Stars: <a href="#">Robert Downey Jr.,</a> <a href="#">Chris Evans,</a> <a href="#">  Chris Hemsworth</a></p>
-					</div>
-				</div>
+				<Pelicula titulo="oblivion (2012)" calificacion="8.1" director="Joss Whedon" 
+        actores="Robert Downey Jr., Chris Evans, Chris Hemsworth" fecha="1 May 2015" duracion="2h21’"
+        img="images/uploads/mv1.jpg">
+          Earth's mightiest heroes must come together and learn to fight as a
+          team if they are to stop the mischievous Loki and his alien army from
+          enslaving humanity...
+        </Pelicula>
 
-        
-				<div className="movie-item-style-2">
-					<img src="images/uploads/mv2.jpg" alt="" />
-					<div className="mv-item-infor">
-						<h6><a href="moviesingle.html">into the wild <span>(2014)</span></a></h6>
-						<p className="rate"><i className="ion-android-star"></i><span>7.8</span> /10</p>
-						<p className="describe">As Steve Rogers struggles to embrace his role in the modern world, he teams up with a fellow Avenger and S.H.I.E.L.D agent, Black Widow, to battle a new threat...</p>
-						<p className="run-time"> Run Time: 2h21’    .     <span>MMPA: PG-13 </span>    .     <span>Release: 1 May 2015</span></p>
-						<p>Director: <a href="#">Anthony Russo,</a><a href="#">Joe Russo</a></p>
-						<p>Stars: <a href="#">Chris Evans,</a> <a href="#">Samuel L. Jackson,</a> <a href="#">  Scarlett Johansson</a></p>
-					</div>
-				</div>
+				<Pelicula titulo="into the wild (2014)" calificacion="7.8" director="Anthony Russo,Joe Russo" 
+        actores="Chris Evans,Samuel L. Jackson,Scarlett Johansson" fecha="1 May 2015" duracion="2h21’"
+        img="images/uploads/mv2.jpg">
+          As Steve Rogers struggles to embrace his role in the modern world, he teams up with a fellow 
+          Avenger and S.H.I.E.L.D agent, Black Widow, to battle a new threat...
+        </Pelicula>
+
+
+				
 				<div className="movie-item-style-2">
 					<img src="images/uploads/mv3.jpg" alt="" />
 					<div className="mv-item-infor">
