3abf420b53c67819b3ae0c3a06ef3c892ec75fd0
frdeoses <fdoses@outlook.es>
2021-12-19 19:29:48 +0100

    Create Route and add Blog

--- src/App.js
+++ src/App.js
@@ -1,57 +1,16 @@
-import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import PageWrapper from "./PageWrapper";
-import Paginacion from "./Paginacion";
-import Pelicula from "./Pelicula";
-import peliculasJSON from "./peliculas.json";
+import Blog from "./views/Blog";
+import ListadoPeliculas from "./views/ListadoPeliculas";
 
 function App() {
-  const [paginaActual, setPaginaActual] = useState(1);
-
-  const TOTAL_POR_PAGINA = 7;
-
-  let peliculas = peliculasJSON;
-
-  const cargarPeliculas = () => {
-    // peliculas = peliculas.slice(0,  5);
-    // peliculas = peliculas.slice(5,  10);
-    peliculas = peliculas.slice(
-      (paginaActual - 1) * TOTAL_POR_PAGINA,
-      paginaActual * TOTAL_POR_PAGINA
-    );
-  };
-
-  const getTotalPagina = () => {
-    let catidadTotalPelis = peliculasJSON.length;
-
-    return Math.ceil(catidadTotalPelis / TOTAL_POR_PAGINA);
-  };
-
-  cargarPeliculas();
   return (
-    <PageWrapper>
-      {peliculas.map((pelicula) => (
-        <Pelicula
-          titulo={pelicula.titulo}
-          calificacion={pelicula.calificacion}
-          director={pelicula.director}
-          actores={pelicula.actores}
-          fecha={pelicula.fecha}
-          duracion={pelicula.duracion}
-          img={pelicula.img}
-        >
-          {pelicula.descripcion}
-        </Pelicula>
-      ))}
-
-      <Paginacion
-        pagina={paginaActual}
-        total={getTotalPagina()}
-        onChange={(pag) => {
-          setPaginaActual(pag);
-        }}
-      />
-    </PageWrapper>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/blog" element={<Blog />}></Route>
+        <Route path="/" element={<ListadoPeliculas />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
